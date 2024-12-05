29e73a81fa1cbcf8cd12000a67ece2c25be59d8c
patodelao <patricio.delao@sansano.usm.cl>
2024-12-05 18:37:52 -0300

    Signed-off-by: patodelao <patricio.delao@sansano.usm.cl>

--- ELO301 FFT/Core/Inc/main.h
+++ ELO301 FFT/Core/Inc/main.h
@@ -59,6 +59,7 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define B1_Pin GPIO_PIN_13
 #define B1_GPIO_Port GPIOC
+#define B1_EXTI_IRQn EXTI15_10_IRQn
 #define TP_Pin GPIO_PIN_1
 #define TP_GPIO_Port GPIOC
 #define USART_TX_Pin GPIO_PIN_2
--- ELO301 FFT/Core/Inc/stm32l4xx_it.h
+++ ELO301 FFT/Core/Inc/stm32l4xx_it.h
@@ -57,6 +57,7 @@ void PendSV_Handler(void);
 void SysTick_Handler(void);
 void DMA1_Channel4_IRQHandler(void);
 void ADC1_IRQHandler(void);
+void EXTI15_10_IRQHandler(void);
 void DFSDM1_FLT0_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
